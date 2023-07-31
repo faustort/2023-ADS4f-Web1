489d6c46cbd8907c5bcc7e5b8ab76887cecb83f4
Your Name <you@example.com>
2023-07-31 20:43:32 -0300

    Init

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title></title>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+</head>
+
+<body>
+    <header> <!-- Meu cabeçalho -->
+        <nav> <!-- Começo do Menu -->
+            <ul> <!-- Lista de links -->
+                <li><a href="index.php">Inicial</a></li>
+                <li><a href="sobre.php">Sobre</a></li>
+                <li><a href="contato.php">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
+    <!-- Conteúdo principal do site -->
+    <main>
+        <h1>Bem vindos ao Meu Site Feliz</h1>
+        <p>Seja bem vindo ao melhor site da américa!</p>
+    </main>
+    <!-- Rodapé do site -->
+    <footer>
+        <p>Aqui é meu rodapé</p>
+    </footer>
+</body>
+
+</html>
