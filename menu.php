36983ce0c88a03015a86515bf37cf13afdb219fa
Your Name <you@example.com>
2023-07-31 21:40:52 -0300

    menu

--- menu.php
+++ menu.php
@@ -1,13 +1,18 @@
     <header class="cabecalho"> <!-- Meu cabeçalho -->
+        <!-- F1 -->
         <div class="logotipo">Logotipo</div>
+
+        <!-- F2 -->
         <nav> <!-- Começo do Menu -->
             <ul class="menu"> <!-- Lista de links -->
                 <li><a href="index.php">Inicial</a></li>
-                <!-- <li><a href="sobre.php">Sobre</a></li> -->
+                <li><a href="sobre.php">Sobre</a></li>
                 <li><a href="portfolio.php">Portifólio</a></li>
                 <li><a href="servicos.php">Serviços</a></li>
                 <li><a href="contato.php">Contato</a></li>
             </ul>
         </nav>
+
+        <!-- F3 -->
         <div class="redes">Redes Sociais</div>
     </header>
