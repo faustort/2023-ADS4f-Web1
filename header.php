a24dc0ea97db95ad88910a13266b6237356a1772
Your Name <you@example.com>
2023-07-31 21:28:11 -0300

    css reset

--- header.php
+++ header.php
@@ -1,18 +1,18 @@
 <?php
 
 if (isset($titulo)) {
-
 } else {
     $titulo = "Título padrão";
 }
 // $titulo = isset($titulo) ? $titulo : "Título padrão";
-?> 
+?>
 <!DOCTYPE html>
 <html>
 
 <head>
     <title><?php echo $titulo;  ?></title>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <link rel="stylesheet" href="assets/css/reset.css">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
