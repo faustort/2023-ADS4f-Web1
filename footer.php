ceee73e921e9d82dfafb27ee0b443c5e776407e0
Your Name <you@example.com>
2023-08-14 21:31:24 -0300

    grid update

--- footer.php
+++ footer.php
@@ -1,15 +1,15 @@
     <!-- Rodapé do site -->
     <footer>
         <div class="grid-container">
-            <div class="col-4">
+            <div class="col-6">
                 <h4>Coluna 1</h4>
                 <p>Aqui é meu rodapé</p>
             </div>
-            <div class="col-4">
+            <div class="col-3">
                 <h4>Coluna 2</h4>
                 <p>Aqui é meu rodapé</p>
             </div>
-            <div class="col-4">
+            <div class="col-3">
                 <h4>Coluna 3</h4>
                 <p>Aqui é meu rodapé</p>
             </div>
