610544dc68f4a8ebd094bba7c2272339f4c1c679
Your Name <you@example.com>
2023-08-18 19:54:14 -0300

    novidades

--- menu.php
+++ menu.php
@@ -1,20 +1,25 @@
-    <header class="cabecalho container"> <!-- Meu cabeçalho -->
-        <!-- F1 -->
-        <div class="logotipo">
-            <a href="index.php"><img src="assets/images/logo.webp"></a>
+    <header class="grid-container"> <!-- Meu cabeçalho -->
+        <div class="col-3">
+            <!-- F1 -->
+            <div class="logotipo">
+                <a href="index.php"><img src="assets/images/logo.webp"></a>
+            </div>
+        </div>
+        <!-- F2 -->
+        <div class="col-6">
+            <nav> <!-- Começo do Menu -->
+                <ul class="menu"> <!-- Lista de links -->
+                    <li><a href="index.php" class="amarelo">Inicial</a></li>
+                    <li><a href="sobre.php">Sobre</a></li>
+                    <li><a href="portfolio.php">Portifólio</a></li>
+                    <li><a href="servicos.php">Serviços</a></li>
+                    <li><a href="contato.php">Contato</a></li>
+                </ul>
+            </nav>
         </div>
 
-        <!-- F2 -->
-        <nav> <!-- Começo do Menu -->
-            <ul class="menu"> <!-- Lista de links -->
-                <li><a href="index.php" class="amarelo">Inicial</a></li>
-                <li><a href="sobre.php">Sobre</a></li>
-                <li><a href="portfolio.php">Portifólio</a></li>
-                <li><a href="servicos.php">Serviços</a></li>
-                <li><a href="contato.php">Contato</a></li>
-            </ul>
-        </nav>
-
         <!-- F3 -->
-        <div class="redes">Redes Sociais</div>
+        <div class="col-3">
+            <div class="redes">Redes Sociais</div>
+        </div>
     </header>
