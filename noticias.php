828496b78d52cf384cdc04c40cbd6f22de0f45b9
Your Name <you@example.com>
2023-08-21 21:52:29 -0300

    noticias

--- noticias.php
+++ noticias.php
@@ -5,17 +5,31 @@ include_once __DIR__ . "/config/connection.php";
 ?>
 
 <main>
-    <h1>Notícias do Senac</h1>
-    <?php
-    $sql = "SELECT * FROM noticias";
-    $resultado = $pdo->query($sql);
-    if ($resultado) {
-        while ($linha = $resultado->fetch(PDO::FETCH_ASSOC)) {
-            echo $linha['titulo'];
-            echo $linha['descricao'];
-        }
-    }
-    ?>
+    <div class="container">
+        <h1>Notícias do Senac</h1>
+        <div class="row">
+            <?php
+            $sql = "SELECT * FROM noticias";
+            $resultado = $pdo->query($sql);
+            if ($resultado) {
+                while ($linha = $resultado->fetch(PDO::FETCH_ASSOC)) {
+                    echo '<div class="col-md-4">';
+                    echo '<div class="card">';
+                    echo '<img class="card-img-top" src="//picsum.photos/500/200" alt="Imagem de capa do card">';
+                    echo '<div class="card-body">';
+                    echo '    <h5 class="card-title">' . $linha['titulo'] . '</h5>';
+                    echo '    <p class="card-text">' . $linha['descricao'] . '</p>';
+                    echo '   <a href="#" class="btn btn-primary">Visitar</a>';
+                    echo '</div>';
+                    echo '</div>';
+                    echo '</div>';
+
+                    
+                }
+            }
+            ?>
+        </div>
+    </div>
 </main>
 
 
