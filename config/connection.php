a3b84806d63075358ecbfb738e7d170cf7e5058e
Your Name <you@example.com>
2023-08-21 21:01:31 -0300

    mysql

---
+++ config/connection.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = 'localhost';
+$username = 'senac';
+$password = 'senac';
+$database = 'aula';
+
+// crio a conexação com o banco de dados 
+$pdo = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+
+// realizo um select básico para testar a conexão
+$sql = "SELECT * FROM noticias";
+
+// executando o sql dentro do banco baseado nas informações de conexão
+$resultado = $pdo->query($sql);
