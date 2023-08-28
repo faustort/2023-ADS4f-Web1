a9e39962712bc10a291cbe25f1e1ba65a1de21ea
Your Name <you@example.com>
2023-08-28 20:27:21 -0300

    form

--- contato.php
+++ contato.php
@@ -4,27 +4,35 @@ include __DIR__ . '/header.php';
 ?>
 
 <!-- Conteúdo principal do site -->
-<main> 
+<main class="mt-5 mb-5">
     <div class="container">
         <div class="row">
             <div class="col-md-6">
                 <h1>Fale comigo</h1>
                 <p>Preencha o formulário</p>
 
-                <div>
+                <form action="" method="get" enctype="multipart/form-data">
                     <div class="mb-3">
                         <label for="nome_input" class="form-label">Digite seu Nome</label>
-                        <input type="text" class="form-control form-control-lg" id="nome_input" placeholder="Digite seu nome completo">
+                        <input type="text" name="nome" class="form-control form-control-lg" id="nome_input" placeholder="Digite seu nome completo">
                     </div>
                     <div class="mb-3">
                         <label for="email_input" class="form-label">Digite seu e-mail</label>
-                        <input type="email" class="form-control form-control-lg" id="email_input" placeholder="Digite o e-mail corretamente">
+                        <input type="email" name="email" class="form-control form-control-lg" id="email_input" placeholder="Digite o e-mail corretamente">
                     </div>
                     <div class="mb-3">
-                        <label for="exampleFormControlTextarea1" class="form-label">Digite sua mensagem</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="4"></textarea>
+                        <label for="mensagem_input" class="form-label">Digite sua mensagem</label>
+                        <textarea class="form-control" name="mensagem" id="mensagem_input" rows="4"></textarea>
                     </div>
-                </div>
+                    <input type="submit" class="btn btn-primary" value="Enviar form">
+                </form>
+            </div>
+            <div class="col-md-6">
+                <?php
+                echo isset($_GET['nome']) ? $_GET['nome'] : 'Nome não informado<br>';
+                echo isset($_GET['email']) ? $_GET['email'] : 'Email não informado<br>';
+                echo isset($_GET['mensagem']) ? $_GET['mensagem'] : 'Mensagem não informada';
+                ?>
             </div>
         </div>
     </div>
