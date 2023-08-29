ca93685c50dafb8ff3a5b8a4b9a94c05dea61ba9
Your Name <you@example.com>
2023-08-28 21:11:55 -0300

    move upload file

--- portfolio.php
+++ portfolio.php
@@ -2,8 +2,25 @@
 $titulo = "Meu portfólio";
 include __DIR__ . '/header.php';
 include_once __DIR__ . '/config/connection.php'; // once: se eu já inclui em algum lugar não dará erro
-?>
 
+
+if (isset($_POST['titulo']) && isset($_POST['descricao']) && isset($_FILES['imagem'])) {
+    $diretorio = __DIR__ . '/assets/images/'; // para onde ela vai
+    $nomeArquivo = $_FILES['imagem']['name']; // nome do arquivo
+    $arquivo = $diretorio . $nomeArquivo; // caminho completo do arquivo
+
+    if(move_uploaded_file($_FILES['imagem']['tmp_name'], $arquivo)){
+        echo 'Arquivo salvo com sucesso!';
+    } else {
+        echo 'Erro ao salvar o arquivo!';
+    }
+
+    $titulo = $_POST['titulo'];
+    $descricao = $_POST['descricao'];
+}
+
+
+?>
 <!-- Conteúdo principal do site -->
 <main class="my-5">
     <div class="container">
@@ -19,12 +36,15 @@ include_once __DIR__ . '/config/connection.php'; // once: se eu já inclui em al
                         <input class="form-control" type="text" name="titulo" id="titulo">
                     </div>
                     <div>
-                        <label class="form-label" for="descricao">Título do Portfólio</label>
+                        <label class="form-label" for="descricao">Descrição</label>
                         <textarea class="form-control" name="descricao" rows="4"></textarea>
                     </div>
                     <div>
                         <label class="form-label" for="imagem">Selecione sua imagem</label>
                         <input class="form-control" type="file" name="imagem" id="imagem">
+                    </div>
+                    <div>
+                        <input class="btn btn-primary" type="submit" value="Enviar">
                     </div>
                 </form>
             </div>
