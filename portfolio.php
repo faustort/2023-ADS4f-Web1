45502aadf77c0d1881ada14c7b4f8d2ecd56b943
Your Name <you@example.com>
2023-08-28 21:37:38 -0300

    port

--- portfolio.php
+++ portfolio.php
@@ -9,14 +9,15 @@ if (isset($_POST['titulo']) && isset($_POST['descricao']) && isset($_FILES['imag
     $nomeArquivo = $_FILES['imagem']['name']; // nome do arquivo
     $arquivo = $diretorio . $nomeArquivo; // caminho completo do arquivo
 
-    if(move_uploaded_file($_FILES['imagem']['tmp_name'], $arquivo)){
+    if (move_uploaded_file($_FILES['imagem']['tmp_name'], $arquivo)) {
+        $titulo = $_POST['titulo'];
+        $descricao = $_POST['descricao'];
+        $pdo->query("INSERT INTO portfolio (titulo, descricao, imagem) VALUES ('$titulo', '$descricao', '$nomeArquivo')");
+
         echo 'Arquivo salvo com sucesso!';
     } else {
         echo 'Erro ao salvar o arquivo!';
     }
-
-    $titulo = $_POST['titulo'];
-    $descricao = $_POST['descricao'];
 }
 
 
@@ -28,9 +29,26 @@ if (isset($_POST['titulo']) && isset($_POST['descricao']) && isset($_FILES['imag
             <div class="col-md-6">
                 <h1>Sobre a minha pessoa</h1>
                 <p>Veja meu currículo</p>
+<?php
+    
+    $resultado = $pdo->query("SELECT * FROM portfolio");
+    if($resultado){
+        while($linha = $resultado->fetch(PDO::FETCH_ASSOC)){
+            echo '<div class="card">';
+            echo '<img class="card-img-top" src="assets/images/' . $linha['imagem'] . '" alt="Imagem de capa do card">';
+            echo '<div class="card-body">';
+            echo '    <h5 class="card-title">' . $linha['titulo'] . '</h5>';
+            echo '    <p class="card-text">' . $linha['descricao'] . '</p>';
+            echo '   <a href="#" class="btn btn-primary">Visitar</a>';
+            echo '</div>';
+            echo '</div>';
+        }
+    }
+
+?>
             </div>
             <div class="col-md-6">
-                <form action="" method="post">
+                <form action="" method="post" enctype="multipart/form-data">
                     <div>
                         <label class="form-label" for="titulo">Título do Portfólio</label>
                         <input class="form-control" type="text" name="titulo" id="titulo">
