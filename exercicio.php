44366f3dac0a8d69c85215fd442eb0bb48d13c48
Your Name <you@example.com>
2023-09-04 21:19:42 -0300

    exercicio completo

--- exercicio.php
+++ exercicio.php
@@ -61,17 +61,43 @@ include __DIR__ . '/header.php';
     });
 
     function adicionarATabela() {
+        // Cria um elemento virtual tr no DOM
         let tr = document.createElement('tr');
+        // Cria dois elementos virtuais td no DOM
         let tdNome = document.createElement('td');
         let tdProfissao = document.createElement('td');
 
+        // Pega os valores dos inputs e coloca dentro dos tds
         tdNome.innerText = document.getElementById('nome').value;
         tdProfissao.innerText = document.getElementById('profissao').value;
 
+        // inicio o processo de criação do botão trash
+        btTrash = document.createElement('i');
+        // adiciono a este elemento virtual as classes do bootstrap icons
+        btTrash.classList.add('bi', 'bi-trash');
+        // adiciono um valor vazio para o botão trash
+        btTrash.innerText = " ";
+
+        // antes de adicionar o botão trash ao tdProfissao, 
+        // preciso adicionar um evento de click
+        btTrash.addEventListener('click', function(e) {
+            // previno o comportamento padrão do botão
+            e.preventDefault();
+            // removo o elemento pai do elemento que foi clicado
+            tr.remove();
+        })
+
+        // finalmente insiro o botão trash dentro do tdProfissao
+        tdProfissao.appendChild(btTrash);
+
+        // pego a tabela e insiro os tds dentro da tr
         const tabela = document.getElementById('tabelaPessoas');
-       
+
+        // insiro os tds dentro da tr
         tr.appendChild(tdNome);
         tr.appendChild(tdProfissao);
+
+        // por final insiro a tr dentro da tabela
         tabela.appendChild(tr);
 
     }
