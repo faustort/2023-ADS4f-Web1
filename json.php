2fce7d702b4703f30947fa8bc4fd9e613365224b
Your Name <you@example.com>
2023-09-11 20:37:39 -0300

    exercicio Json

---
+++ json.php
@@ -0,0 +1,14 @@
+<?php
+$titulo = "Exercício Json";
+include __DIR__ . '/header.php';
+?>
+
+<main>
+    <div class="container">
+        <h1>Exercício Json</h1>
+    </div>
+</main>
+
+<?php
+include __DIR__ . '/footer.php';
+?>
--- menu.php
+++ menu.php
@@ -1,13 +1,13 @@
 <div class="fundo-gradiente">
     <header class="container cabecalho"> <!-- Meu cabeçalho -->
-        <div class="col-3">
+        <div class="col-2">
             <!-- F1 -->
             <div class="logotipo">
                 <a href="index.php"><img src="assets/images/logo.webp"></a>
             </div>
         </div>
         <!-- F2 -->
-        <div class="col-6">
+        <div class="col-7">
             <nav> <!-- Começo do Menu -->
                 <ul class="menu"> <!-- Lista de links -->
                     <li><a href="index.php" class="amarelo">Inicial</a></li>
@@ -16,6 +16,7 @@
                     <li><a href="noticias.php">Notícias</a></li>
                     <li><a href="servicos.php">Serviços</a></li>
                     <li><a href="exercicio.php">Exercício</a></li>
+                    <li><a href="json.php">Json</a></li>
                     <li><a href="contato.php">Contato</a></li>
                 </ul>
             </nav>
@@ -23,7 +24,7 @@
 
         <!-- F3 -->
         <div class="col-3">
-            <div class="redes">Redes Sociais</div>
+            <div class="redes"><i class="bi bi-facebook"></i></div>
         </div>
     </header>
 </div>
