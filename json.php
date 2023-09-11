99325fc96716a62665599ef0ae3c669842478779
Your Name <you@example.com>
2023-09-11 20:49:55 -0300

    json inicial

--- json.php
+++ json.php
@@ -6,8 +6,23 @@ include __DIR__ . '/header.php';
 <main>
     <div class="container">
         <h1>Exercício Json</h1>
+        <div id="noticias">
+            Lista de notícias
+        </div>
     </div>
 </main>
+
+<script>
+    let dados = fetch('./rest/index.php')
+        .then(response => response.json())
+        .then(data => criarNoticias(data));
+
+    function criarNoticias(dados) {
+        let noticias = document.getElementById('noticias');
+        // inserir estes resultados dentro da DIV Notícias
+
+    }
+</script>
 
 <?php
 include __DIR__ . '/footer.php';
