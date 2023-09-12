bd661064f4979acca36bb74cabf6180009cf575f
Your Name <you@example.com>
2023-09-11 21:47:19 -0300

    path definido

--- rest/index.php
+++ rest/index.php
@@ -1,6 +1,19 @@
 <?php
 // define o cabeçalho da resposta do servidor como Json
 header('Content-Type: application/json');
+
+// verifica se o parâmetro path foi enviado
+// se sim, retorna um Json com o caminho para fins de depuração
+if (isset($_GET['path'])) {
+    echo json_encode(
+        [
+            'path' => $_GET['path'],
+            'titulo' => 'Estamos usando o caminho acima',
+        ]
+    );
+    exit;
+}
+
 // inclui o arquivo de conexão com o banco de dados
 include __DIR__ . './../config/connection.php';
 // faz a consulta no banco de dados
